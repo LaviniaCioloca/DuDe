9d44d07987df4420f93a9f03cdb3f92c60e08a74
Lavinia Cioloca <lavinia.cioloca@brivo.com>
2020-06-08 12:34:04 +0300

    [fix] Fix error in script due to values not being on the same line as printf

--- src/main/resources/DuDe-analysis.sh
+++ src/main/resources/DuDe-analysis.sh
@@ -13,22 +13,19 @@ printf "\n--- DuDeJenkinsPlugin result ---\n"
 if (( $(echo "$percentageChange > $maximum_duplication_percentage_increase_allowed" | bc -l) )) && (( $(echo "$currentPercentage > $maximum_duplication_percentage_in_project" | bc -l) ));
 then
   {
-    printf "ERROR! Duplication percentage increase %s is greater than %s and the current duplication percentage %s exceeds the threshold value %s \n\n"
-    "$percentageChange" "$maximum_duplication_percentage_increase_allowed" "$currentPercentage" "$maximum_duplication_percentage_in_project" ;
+    printf "ERROR! Duplication percentage increase %s is greater than %s and the current duplication percentage %s exceeds the threshold value %s \n\n" "$percentageChange" "$maximum_duplication_percentage_increase_allowed" "$currentPercentage" "$maximum_duplication_percentage_in_project" ;
     exit 1;
   }
 elif (( $(echo "$percentageChange > $maximum_duplication_percentage_increase_allowed" | bc -l) ));
 then
   {
-    printf "ERROR! Duplication percentage increase %s is greater than %s \n\n" ;
-    "$percentageChange" "$maximum_duplication_percentage_increase_allowed"
+    printf "ERROR! Duplication percentage increase %s is greater than the threshold value %s \n\n" "$percentageChange" "$maximum_duplication_percentage_increase_allowed" ;
     exit 1;
   }
 elif (( $(echo "$currentPercentage > $maximum_duplication_percentage_in_project" | bc -l) ));
 then
   {
-    printf "ERROR! The current duplication percentage %s exceeds the threshold value %s \n\n" ;
-    "$currentPercentage" "$maximum_duplication_percentage_in_project"
+    printf "ERROR! The current duplication percentage %s exceeds the threshold value %s \n\n" "$currentPercentage" "$maximum_duplication_percentage_in_project" ;
     exit 1;
   }
 else
